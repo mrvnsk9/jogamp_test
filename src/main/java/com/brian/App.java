76e36f77d4b39d6e13024b5ac2a5ee3da4aafc2a
Brian <mrvnsk9@gmail.com>
2014-10-03 10:11:58 -0500

    Initial Commit

---
+++ src/main/java/com/brian/App.java
@@ -0,0 +1,42 @@
+package com.brian;
+
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
+
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLProfile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static {
+        GLProfile.initSingleton();
+    }
+
+    public void run() {
+//        GLProfile glp = GLProfile.getDefault();
+        GLProfile glp = GLProfile.get(GLProfile.GL2);
+        GLCapabilities caps = new GLCapabilities(glp);
+
+        GLWindow window = GLWindow.create(caps);
+        window.setSize(800, 600);
+        window.setVisible(true);
+        window.setTitle("NEWT Window Test");
+
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDestroyNotify(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main( String[] args )
+    {
+        new App().run();
+    }
+}
