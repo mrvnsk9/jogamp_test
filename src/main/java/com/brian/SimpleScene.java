18bea4b78383494505911c578ba9b634e004237c
Brian <mrvnsk9@gmail.com>
2014-10-03 12:23:36 -0500

    test

--- src/main/java/com/brian/SimpleScene.java
+++ src/main/java/com/brian/SimpleScene.java
@@ -1,5 +1,8 @@
 package com.brian;
 
+import com.jogamp.opengl.util.Animator;
+import com.jogamp.opengl.util.FPSAnimator;
+
 import javax.media.opengl.*;
 import javax.media.opengl.awt.GLCanvas;
 import java.awt.*;
@@ -8,13 +11,17 @@ import java.awt.*;
  * Created by brian on 10/3/14.
  */
 public class SimpleScene implements GLEventListener {
+    private double theta = 0;
+    private double s = 0;
+    private double c = 0;
+
     static {
         GLProfile.initSingleton();
     }
 
     @Override
     public void init(GLAutoDrawable glAutoDrawable) {
-
+        glAutoDrawable.getGL().setSwapInterval(1);
     }
 
     @Override
@@ -30,11 +37,12 @@ public class SimpleScene implements GLEventListener {
 
     @Override
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i2, int i3, int i4) {
-
+        theta += 0.01;
+        s = Math.sin(theta);
+        c = Math.cos(theta);
     }
 
     private void update() {
-
     }
 
     private void render(GLAutoDrawable glAutoDrawable) {
@@ -42,11 +50,11 @@ public class SimpleScene implements GLEventListener {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
         gl.glBegin(GL.GL_TRIANGLES);
         gl.glColor3f(1, 0, 0);
-        gl.glVertex2f(-1, -1);
+        gl.glVertex2f(-(float)c, -(float)c);
         gl.glColor3f(0, 1, 0);
-        gl.glVertex2f(0, 1);
+        gl.glVertex2f(0, (float)c);
         gl.glColor3f(0, 0, 1);
-        gl.glVertex2f(1, -1);
+        gl.glVertex2f((float)s, -(float)s);
         gl.glEnd();
     }
 
@@ -68,5 +76,9 @@ public class SimpleScene implements GLEventListener {
         });
 
         canvas.addGLEventListener(new SimpleScene());
+
+        GLAnimatorControl animator = new Animator(canvas);
+        animator.add(canvas);
+        animator.start();
     }
 }
